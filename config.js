bd67a1025e743ae900a8a2e0c70f5ff049debc36
ali-lasharivj <queensapna26@gmail.com>
2025-06-17 09:41:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 const fs = require('fs');
-const path = require('path');
-const { getConfig } = require("./lib/configdb");
-
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
@@ -9,68 +6,65 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-//chatbot integration 
-CHATBOT: getConfig("CHATBOT") || "on",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*sᴛᴀᴛᴜs sᴜᴄᴄᴇssғᴜʟʟʏ ᴠɪᴇᴡᴇᴅ ʙʏ x-ʙᴏᴛ-ᴍᴅ*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS JUST NOW ALI-MD 👻*",
 // set the auto reply massage on status reply  
-WELCOME: process.env.WELCOME || "true",
-// true if want welcome and goodbye msg in groups    
-ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
-// make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "false",
-//anticall integration 
-ANTI_CALL: getConfig("ANTI_CALL") || "true",
-// make anti link true,false for groups 
-MENTION_REPLY: process.env.MENTION_REPLY || "false",
-// make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/yv8zy4.jpg" ,
-// add custom menu and mention reply image url
-PREFIX: getConfig("PREFIX") || ".", 
+PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "X-BOT-MD",
+BOT_NAME: process.env.BOT_NAME || "ALI-MD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "X-BOT-MD",
-    
-VPS: process.env.VPS || "",
+STICKER_NAME: process.env.STICKER_NAME || "ALI-MD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
+OWNER_REACT: process.env.OWNER_REACT || "false",
+// make this true for owner emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "true",
-// automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "2349133354644",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923003588997",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Dᴀᴠɪᴅ X",
-// add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Dᴀᴠɪᴅ X*",
+OWNER_NAME: process.env.OWNER_NAME || "ALI",
+// add bot owner name 
+WELCOME: process.env.WELCOME || "true",
+// true if want welcome msg in groups
+GOODBYE: process.env.GOODBYE || "true",
+// true if want goodbye msg in groups
+ADMIN_STATUS: process.env.ADMIN_STATUS || "false",
+// make true to know who dismiss or promoted a member in group
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+// maks true for always online 
+MENTION_REPLY: process.env.MENTION_REPLY || "false",
+// maks true for always online 
+ANTI_CALL: process.env.ANTI_CALL || "false",
+//maks anticall true, false for calls
+REJECT_MSG: process.env.REJECT_MSG || "*📞 ᴄαℓℓ ɴσт αℓℓσωє∂ ιɴ тнιѕ ɴᴜмвєʀ уσυ ∂σɴт нανє ᴘєʀмιѕѕισɴ 📵*", 
+// add anticall message 
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘσωєʀє∂ ву αℓι м∂⎯꯭̽💀*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/yv8zy4.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://cdn.ironman.my.id/i/2du3i5.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> 's T100^607T708X",
+LIVE_MSG: process.env.LIVE_MSG || "> HEY 👋 IM ALIVE NOW*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
 AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
-// false or true for anti bad words 
-ANTI_BOT: process.env.ANTI_BOT || "true",
-MODE: getConfig("MODE") || process.env.MODE || "public",
+ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "true",
+// false or true for anti bad words  
+MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
 ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
 // make anti link true,false for groups 
-ANTIVIEW_ONCE: process.env.ANTIVIEW_ONCE || "off",
-ANTILINK_WARN: process.env.ANTILINK_WARN || "false",
-ANTILINK_KICK: process.env.ANTILINK_KICK || "false",
-ANTILINK: process.env.ANTILINK || "false",
+ANTI_LINK_DELETE: process.env.ANTI_LINK_DELETE || "false",
+// make anti link true,false for groups 
+ANTI_LINK_WARN: process.env.ANTI_LINK_WARN || "false",
+// make anti link true,false for groups 
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
@@ -85,13 +79,15 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "2349133354644",
+DEV: process.env.DEV || "923003588997",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
-ANTI_DELETE: process.env.ANTI_DELETE || "false",
+ANTI_DELETE: process.env.ANTI_DELETE || "true",
+// set true false for anti delete     
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
-// change it to 'same' if you want to resend deleted message in same chat 
+// change it to 'same' if you want to resend deleted message in same chat     
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
 // make it true for auto recoding 
 };
+  
