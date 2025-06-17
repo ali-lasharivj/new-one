f5be9186716f1f16b8038a7548cb1fcb5cbbb731
ali-lasharivj <queensapna26@gmail.com>
2025-06-17 08:18:24 +0500

    Create yt-play.js

---
+++ plugins/yt-play.js
@@ -0,0 +1,104 @@
+const config = require('../config');
+const { cmd } = require('../command');
+const { ytsearch } = require('@dark-yasiya/yt-dl.js');
+
+// MP4 video download
+
+cmd({ 
+    pattern: "mp4", 
+    alias: ["video","ytv"], 
+    react: "🎥", 
+    desc: "Download YouTube video", 
+    category: "main", 
+    use: '.mp4 < Yt url or Name >', 
+    filename: __filename 
+}, async (conn, mek, m, { from, prefix, quoted, q, reply }) => { 
+    try { 
+        if (!q) return await reply("*𝐏ℓєαʂє 𝐏ɼ๏νιɖє 𝐀 𝐘ʈ 𝐔ɼℓ ๏ɼ 𝐕ιɖє๏ 𝐍αмє..*");
+        
+        const yt = await ytsearch(q);
+        if (yt.results.length < 1) return reply("No results found!");
+        
+        let yts = yt.results[0];  
+        let apiUrl = `https://apis.davidcyriltech.my.id/download/ytmp4?url=${encodeURIComponent(yts.url)}`;
+        
+        let response = await fetch(apiUrl);
+        let data = await response.json();
+        
+        if (data.status !== 200 || !data.success || !data.result.download_url) {
+            return reply("Failed to fetch the video. Please try again later.");
+        }
+
+        let ytmsg = `*╭┈❍「 𝐀ɭīī 𝐌Ɗ 𝐕𝐈𝐃𝐄𝐎 𝐃-𝐋 」❍*
+*│🎡 TITLE:* ${yts.title}
+*│⏰ DURATION:* ${yts.timestamp}
+*│👀 VIEWS:* ${yts.views}
+*│🫟 AUTHOR:* ${yts.author.name}
+‎*╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈✑*
+*ғꪮʀ ʏꪮꪊ ғꪮʀ ᴀʟʟ ꪮғ ᴀꜱ 🍉*`;
+        
+
+        // Send thumbnail with options
+        const videoMsg = await conn.sendMessage(from, { image: { url: yts.thumbnail }, caption: ytmsg }, { quoted: mek });
+
+        
+                        await conn.sendMessage(from, {
+                            video: { url: data.result.download_url },
+                            mimetype: "video/mp4",
+                         }, { quoted: mek });
+        
+    } catch (e) {
+        console.log(e);
+        reply("An error occurred. Please try again later.");
+    }
+});
+
+   
+// MP3 song download 
+
+cmd({ 
+    pattern: "yta", 
+    alias: ["play", "audio"], 
+    react: "🎧", 
+    desc: "Download YouTube song", 
+    category: "main", 
+    use: '.song <query>', 
+    filename: __filename 
+}, async (conn, mek, m, { from, sender, reply, q }) => { 
+    try {
+        if (!q) return reply("*𝐏ℓєα𝐬֟፝є 𝐏ʀ๏νιɖє 𝐀 𝐒๏ƞ͛g 𝐍αмє..*");
+
+        const yt = await ytsearch(q);
+        if (!yt.results.length) return reply("No results found!");
+
+        const song = yt.results[0];
+        const apiUrl = `https://apis.davidcyriltech.my.id/youtube/mp3?url=${encodeURIComponent(song.url)}`;
+        
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+
+        if (!data?.result?.downloadUrl) return reply("Download failed. Try again later.");
+
+    await conn.sendMessage(from, {
+    audio: { url: data.result.downloadUrl },
+    mimetype: "audio/mpeg",
+    fileName: `${song.title}.mp3`,
+    contextInfo: {
+        externalAdReply: {
+            title: song.title.length > 25 ? `${song.title.substring(0, 22)}...` : song.title,
+            body: "⇆  ||◁◁ㅤ ❚❚ ㅤ▷▷||ㅤ ⇆",
+            mediaType: 1,
+            thumbnailUrl: song.thumbnail.replace('default.jpg', 'hqdefault.jpg'),
+            sourceUrl: 'https://youtube.com/watch?v=OMoU0Pfibc4',
+            mediaUrl: 'https://youtube.com/watch?v=OMoU0Pfibc4',
+            showAdAttribution: true,
+            renderLargerThumbnail: false
+        }
+    }
+}, { quoted: mek });
+
+    } catch (error) {
+        console.error(error);
+        reply("An error occurred. Please try again.");
+    }
+});
