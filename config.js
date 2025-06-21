f312a2ed39f06d07a24c3bacb0869e72eed51707
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <alirazalashari457@gmail.com>
2025-06-21 19:45:39 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",  
 //chatbot integration 
