06544ddef3383c28a29cba22905d2b4c9854f403
ali-lasharivj <queensapna26@gmail.com>
2025-07-09 00:01:21 +0500

    Update antinewsletter.js

--- plugins/antinewsletter.js
+++ plugins/antinewsletter.js
@@ -15,7 +15,7 @@ cmd(
     alias: ["anti-channel","antichannel"],
     react: "🔐",
     desc: "Configure anti‑newsletter mode: delete, warn, kick, off",
-    category: "moderation",
+    category: "🪀 group 🪀",
     filename: __filename,
   },
   async (conn, mek, m, { from, args, reply, isGroup, isBotAdmins, isAdmins }) => {
